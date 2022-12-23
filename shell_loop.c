0dfe5534c69760bb2bd95fa2b225f7dc93e8cc24
Johndelsin <johndelsin22@gmail.com>
2022-12-23 06:21:51 -0800

    Simple Shell

--- shell_loop.c
+++ shell_loop.c
@@ -46,7 +46,8 @@ int hsh(info_t *info, char **av)
 /**
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
- * @info: the parameter & return info struct
+ *
+ * Return: -1 if builtin not found,
  * 0 if builtin executed successfully,
  * 1 if builtin found but not successful,
  * -2 if builtin signals exit()
